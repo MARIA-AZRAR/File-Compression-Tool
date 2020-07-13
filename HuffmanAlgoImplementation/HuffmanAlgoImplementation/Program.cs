6885c9ba5715e99d60fd4d24bdf9622f9fc88f4e
MariaAzrar <mariaazrar2000@gmail.com>
2020-07-13 07:45:50 +0500

    Setting up Environment

---
+++ HuffmanAlgoImplementation/HuffmanAlgoImplementation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HuffmanAlgoImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Program myComp = new Program(); //creating instance of the main file
+             string S;       //Enter string to find the huffman code
+             S = Console.ReadLine();
+             Console.WriteLine("String Entered: " + S);
+
+             Console.ReadKey();
+        }
+    }
+}
