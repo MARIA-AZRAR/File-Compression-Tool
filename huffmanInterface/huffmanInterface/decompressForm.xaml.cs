5dd5aeadbd70393068bd6de70f330679f6ca7ea0
sidrakhandev <sidrakhan.me.100@gmail.com>
2020-07-25 23:49:00 +0500

    📦NEW: Added Interface Implementation

---
+++ huffmanInterface/huffmanInterface/decompressForm.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace huffmanInterface
+{
+    /// <summary>
+    /// Interaction logic for decompressForm.xaml
+    /// </summary>
+    public partial class decompressForm : Window
+    {
+        public decompressForm()
+        {
+            InitializeComponent();
+        }
+        private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+            //Set tooltip visibility
+            if (Tb_Btn.IsChecked == true)
+            {
+                tt_home.Visibility = Visibility.Collapsed;
+                tt_compress.Visibility = Visibility.Collapsed;
+                tt_decompress.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                tt_home.Visibility = Visibility.Visible;
+                tt_compress.Visibility = Visibility.Visible;
+                tt_decompress.Visibility = Visibility.Visible;
+            }
+        }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+        private void Tb_Btn_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
+
